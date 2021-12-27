abaa0d0d263e166ac4c8a2d284eb4b88e122a1b3
Liridon Loku <liridonloku@gmail.com>
2021-12-27 13:39:27 +0100

    Add ability to get click coordinates in a screen-size resposive way

--- src/components/ImageComponent.jsx
+++ src/components/ImageComponent.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 import game from "../images/game.jpg";
+import { StyledImageContainer } from "./styles/ImageComponent.styled";
 
 const ImageComponent = () => {
+  const getCoordinates = (e) => {
+    let xCoord = e.nativeEvent.offsetX;
+    let yCoord = e.nativeEvent.offsetY;
+    let imgWidth = e.target.offsetWidth;
+    let imgHeight = e.target.offsetHeight;
+    let xInPercent = xCoord / imgWidth;
+    let yInPercent = yCoord / imgHeight;
+    if (
+      xInPercent >= 0.212 &&
+      xInPercent <= 0.2458 &&
+      yInPercent >= 0.3466 &&
+      yInPercent <= 0.3763
+    ) {
+      console.log("Congrats! You found Arthur.");
+    }
+    console.log(xCoord, yCoord, imgWidth, imgHeight);
+  };
+
   return (
-    <div className="image-container">
-      <img src={game} alt="game" />
-    </div>
+    <StyledImageContainer>
+      <img src={game} alt="game" onClick={(e) => getCoordinates(e)} />
+    </StyledImageContainer>
   );
 };
 
---
+++ src/components/styles/ImageComponent.styled.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const StyledImageContainer = styled.div`
+  & img {
+    width: 100%;
+  }
+`;
