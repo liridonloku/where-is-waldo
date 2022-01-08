00bbe8f4fd28f0ad7799f695dbc00d7c76a07784
Liridon Loku <liridonloku@gmail.com>
2022-01-08 16:20:46 +0100

    Add timer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { StyledHeader } from "./styles/Header.styled";
 
-const Header = () => {
+const Header = ({ time }) => {
+  let displayTime = `${Math.floor(time / 3600)
+    .toString()
+    .padStart(2, "0")}:${Math.floor((time % 3600) / 60)
+    .toString()
+    .padStart(2, "0")}:${(time % 60).toString().padStart(2, "0")}`;
+
   return (
     <StyledHeader>
       <h1>Where's Waldo? PS4 Edition</h1>
+      <p>{displayTime}</p>
     </StyledHeader>
   );
 };
